b761dd9aeca4fe4b9a0f41f7f49833cf66d3c3c1
hezhichao5 <hezhchao@jd.com>
2018-10-11 21:11:54 +0800

    test

--- app/src/main/java/com/example/hezhichao/gittest/TestUtil.java
+++ app/src/main/java/com/example/hezhichao/gittest/TestUtil.java
@@ -1,4 +1,11 @@
 package com.example.hezhichao.gittest;
 
+import android.util.Log;
+
 public class TestUtil {
+    public static String TAG = TestUtil.class.getSimpleName();
+    public static void test() {
+        Log.v(TAG, "ceshi");
+
+    }
 }
